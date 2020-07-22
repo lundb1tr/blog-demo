70a7447b6eaa7333555e5d357135779f3e421dc8
Tyler Lundberg <tylerl@fireflyint.com>
2020-07-22 16:56:26 -0400

    creating the additional pages

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import Index from './src/screens/IndexScreen';
 import { Provider } from './src/context/BlogContext';
+import Index from './src/screens/IndexScreen';
+import Show from './src/screens/ShowScreen';
+import Create from './src/screens/CreateScreen';
+import Edit from './src/screens/EditScreen';
 
 const navigator = createStackNavigator(
   {
     Index,
+    Show,
+    Create,
+    Edit,
   },
   {
     initialRoutName: 'Index',
---
+++ src/screens/CreateScreen.js
@@ -0,0 +1,46 @@
+import React, { useContext, useState } from 'react';
+import { View, Text, TextInput, StyleSheet, Button } from 'react-native';
+import { Context } from '../context/BlogContext';
+
+const CreateScreen = ({ navigation }) => {
+  const [title, setTitle] = useState('');
+  const [content, setContent] = useState('');
+  return (
+    <View>
+      <Text style={styles.label}>Enter Title</Text>
+      <TextInput
+        style={styles.input}
+        value={title}
+        placeholder="Enter Title"
+        onChangeText={text => setTitle(text)}
+      />
+      <Text style={styles.label}>Enter Content</Text>
+      <TextInput
+        style={styles.input}
+        value={content}
+        placeholder="Enter Content"
+        onChangeText={text => setContent(text)}
+      />
+      <Button title="Add Blog Post" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    fontSize: 18,
+    borderWidth: 1,
+    borderColor: 'black',
+    marginBottom: 15,
+    padding: 5,
+    margin: 5,
+    borderRadius: 10,
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5,
+    marginLeft: 5,
+  },
+});
+
+export default CreateScreen;
